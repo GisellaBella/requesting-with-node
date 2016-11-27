e45065ebf9e0c82793a52d1b9bf60df5a21ef9c2
Gisellabella <gisella@gmail.com>
2016-11-26 21:47:17 -0700

    latest

--- movies.js
+++ movies.js
@@ -1,22 +1,21 @@
 var request = require("request");
-var url = "http://www.omdbapi.com/?t=";//t for movie title
-var requestInfo = "&y=&plot=short&r=json";
-var threeFavorites = ["GroundHog Day", "Ferris Bueller's Day Off", "The Fight Club"];
 var http = require("http");
+var requestInfo = "&y=&plot=short&r=json";
+request.url = "http://www.omdbapi.com/?t=";//t for movie title
+
+var threeFavorites = ["GroundHog Day", "Ferris Bueller's Day Off", "The Fight Club"];
 
 threeFavorites.forEach(function(movie){
-		console.log(http.get("http://www.omdbapi.com/?t=") + movie + "&y=&plot=short&r=json");
+		//console.log(http.get("http://www.omdbapi.com/?t=") + movie + "&y=&plot=short&r=json");
 		var request=http.get("http://www.omdbapi.com/?t=") + movie + "&y=&plot=short&r=json";
-		console.log(request);
-		var movieInfo = body;
-		console.log(movieInfo);
-		});
+		request.success = function(response){
+		console.log(response);
+		var body= JSON.parse(body);
+		console.log(body);
+		};
+});	
 			
 			
 	//http://www.omdbapi.com/?t=ferris bueller's day off&y=&plot=short&r=json
 
 		
-			
-
-
-
