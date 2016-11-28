489352432f1e216a0d8e9956433d23b312ad1764
Gisellabella <gisella@gmail.com>
2016-11-27 21:14:40 -0700

    can't figure out how to get more data back then just the titles.

--- movies.js
+++ movies.js
@@ -1,21 +1,24 @@
-var request = require("request");
-var http = require("http");
-var requestInfo = "&y=&plot=short&r=json";
-request.url = "http://www.omdbapi.com/?t=";//t for movie title
 
 var threeFavorites = ["GroundHog Day", "Ferris Bueller's Day Off", "The Fight Club"];
+var movie =[];
+var body = [];
+var http = require('http');
+var request = require("request");
+
+var url = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json.json";
 
 threeFavorites.forEach(function(movie){
-		//console.log(http.get("http://www.omdbapi.com/?t=") + movie + "&y=&plot=short&r=json");
-		var request=http.get("http://www.omdbapi.com/?t=") + movie + "&y=&plot=short&r=json";
-		request.success = function(response){
-		console.log(response);
-		var body= JSON.parse(body);
-		console.log(body);
-		};
-});	
-			
-			
-	//http://www.omdbapi.com/?t=ferris bueller's day off&y=&plot=short&r=json
-
-		
+	console.log(movie);
+	var request=http.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json");
+			function response() {	
+				var data = '';
+				data = data.toJSON();
+				response.on('data', function(chunk) {//couldnt' fiure out how to get the data back..."chunking" seemed the recommended method, but i may be missing a step, or have immplemented incoorectly.Can only get the titles back.
+				console.log (chunk);	
+				data += chunk;
+				});
+				response.on('end', function() {
+				callback(null, JSON.parse(data));
+				});
+			}
+	});
