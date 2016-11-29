b0635f56cae9ce788182d0b463559f49aca8d743
Gisellabella <gisella@gmail.com>
2016-11-29 00:23:58 -0700

    added app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+
+
+var threeFavorites = require('./movies.js');
+
+console.log(threeFavorites);
--- movies.js
+++ movies.js
@@ -22,3 +22,5 @@ threeFavorites.forEach(function(movie){
 				});
 			}
 	});
+
+module.exports = threeFavorites;
