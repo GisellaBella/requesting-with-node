ea0d99dc9aa693800d6ff697c210104d31a85a0b
Gisellabella <gisella@gmail.com>
2016-11-29 17:13:16 -0700

    made changes, simplified request js, and added the app.js.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
+var movie = require('./movies.js');
+var threeFavorites = ["GroundHog Day", "Ferris Bueller's Day Off", "Overboard"];
+threeFavorites.forEach (function(film){
+movie(film);
+});
 
-var threeFavorites = require('./movies.js');
 
-console.log(threeFavorites);
--- movies.js
+++ movies.js
@@ -1,26 +1,13 @@
 
-var threeFavorites = ["GroundHog Day", "Ferris Bueller's Day Off", "The Fight Club"];
-var movie =[];
-var body = [];
-var http = require('http');
+
 var request = require("request");
 
-var url = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json.json";
+	var movie=function (movieTitle) { 
+		var movieInfo = request("http://www.omdbapi.com/?t=" + movieTitle, function(err,res, body) {
+			var answer = JSON.parse(body);
+			console.log (answer.Title );
+		
+			});
+		};
 
-threeFavorites.forEach(function(movie){
-	console.log(movie);
-	var request=http.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json");
-			function response() {	
-				var data = '';
-				data = data.toJSON();
-				response.on('data', function(chunk) {//couldnt' fiure out how to get the data back..."chunking" seemed the recommended method, but i may be missing a step, or have immplemented incoorectly.Can only get the titles back.
-				console.log (chunk);	
-				data += chunk;
-				});
-				response.on('end', function() {
-				callback(null, JSON.parse(data));
-				});
-			}
-	});
-
-module.exports = threeFavorites;
+module.exports = movie;
